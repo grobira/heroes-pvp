bfe3eddb859d1daeeffca60003ecfaacd6a757b6
giovanni.robira <001002qwe>
2018-08-10 16:33:11 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);  
+  
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 3003,
+    }
+  });
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3004);
+}
+bootstrap();
