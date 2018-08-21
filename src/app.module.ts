c745227106488712aa7d9311729ed6f4cdd36ee7
giovanni.robira <001002qwe>
2018-08-21 13:47:59 -0300

    Fix path that crashes build for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from 'app.controller';
-import { AppService } from 'app.service';
-import { BattleController } from 'battle/battle.controller';
-import { BattleService } from 'battle/battle.service';
-import { HeroRepository } from 'battle/hero.repository';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BattleController } from './battle/battle.controller';
+import { BattleService } from './battle/battle.service';
+import { HeroRepository } from './battle/hero.repository';
 
 @Module({
-  imports: [],
   controllers: [AppController, BattleController],
   providers: [AppService, BattleService, HeroRepository],
 })
