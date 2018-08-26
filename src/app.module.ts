633ed341cfa7119aff3b126a92829ca8738dd807
grobira <grobira2@gmail.com>
2018-08-26 19:26:19 -0300

    Removed app service not been used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BattleModule } from 'battle/battle.module';
 import { BattleReportModule } from 'battleReport/battleReport.module';
@@ -8,6 +7,6 @@ import { BattleReportModule } from 'battleReport/battleReport.module';
 @Module({
   imports : [MongooseModule.forRoot('mongodb://localhost:27017/heroes'), BattleModule, BattleReportModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
